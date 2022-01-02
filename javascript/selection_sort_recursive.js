86528d45f4845b713f4c5669e4776671b21a0735
shu Lin <bbqlinshu@gmail.com>
2022-01-02 13:38:12 -0500

    Completed assignment

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,11 +1,26 @@
 function selectionSortRecursive(arr) {
   // type your code here
+  if(arr.length===0){
+    return []
+  }else{
+    const min = Math.min(...arr)
+    const index = arr.indexOf(min)
+    arr.splice(index, 1)
+    const result = selectionSortRecursive(arr)
+    result.unshift(min)
+    return result
+  }
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
+
+  console.log("");
+
+  console.log("Expecting: [-5, -1, 0, 2, 3]");
+  console.log("=>", selectionSortRecursive([3, -1, -5, 2, 0]));
 
   console.log("");
 }
